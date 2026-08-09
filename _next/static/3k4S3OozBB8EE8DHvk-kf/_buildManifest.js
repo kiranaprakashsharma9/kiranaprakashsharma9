b2c0633765a949d7710060574f5a533496fdc244
RAVICHANDRALS <cravi7715@gmail.com>
2026-08-09 12:15:11 +0530

    Updates

---
+++ _next/static/3k4S3OozBB8EE8DHvk-kf/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/kiranaprakashsharma//_next/:path+",
+        "destination": "/kiranaprakashsharma/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
